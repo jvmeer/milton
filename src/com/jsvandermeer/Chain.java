ba020c0aec61e06bdd23c0dd84b5732bfb978a57
jvmeer <jsvmeer@gmail.com>
2017-03-19 16:25:55 -0400

    stringToDate working

--- src/com/jsvandermeer/Chain.java
+++ src/com/jsvandermeer/Chain.java
@@ -1,23 +1,42 @@
 package com.jsvandermeer;
 
 
+import com.bloomberglp.blpapi.Message;
+import com.bloomberglp.blpapi.MessageIterator;
+
 import java.time.ZonedDateTime;
 import java.util.HashMap;
 
 /**
  * Created by Jacob on 3/18/2017.
  */
+
 public class Chain {
     ZonedDateTime asOf;
-    HashMap<ZonedDateTime, HashMap<String, Market>> chain;
+    HashMap<ZonedDateTime, Link> chain = null;
+    double spot;
 
-
+    private class Link {
+        double forward;
+        HashMap<String, Market> strip;
+    }
 
     private class Market {
         double bidPrice;
         double askPrice;
         long bidSize;
         long askSize;
+    }
+
+
+    public void setAsOf(ZonedDateTime asOf) {
+        this.asOf = asOf;
+    }
+
+    public void setSymbols(MessageIterator iter) {
+        while (iter.hasNext()) {
+            Message message = iter.next();
+        }
     }
 
 }
--- src/com/jsvandermeer/Main.java
+++ src/com/jsvandermeer/Main.java
@@ -1,8 +1,71 @@
 package com.jsvandermeer;
 
+
+import com.bloomberglp.blpapi.*;
+
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
 public class Main {
 
+    private static final String VIX_TICKER = "VIX Index";
+    private static final String SPX_TICKER = "SPX Index";
+    private static final String EXPIRY_TIME = "131500";
+    private static final String TIME_ZONE = "America/Chicago";
+
     public static void main(String[] args) {
+        Session session = createSession();
+        Service service = session.getService("//blp/refdata");
+
+        ZonedDateTime test = stringToDate("20170321");
+        System.out.println(test.toString());
+
+
+        Chain chain = new Chain();
         System.out.println("Hello world!");
+    }
+
+    private static Session createSession() {
+        SessionOptions sessionOptions = new SessionOptions();
+        sessionOptions.setServerHost("localhost");
+        sessionOptions.setServerPort(8194);
+        Session session = new Session(sessionOptions);
+
+        try {
+            session.start();
+        } catch (IOException | InterruptedException exception) {
+            exception.printStackTrace();
+        }
+        try {
+            session.openService("//blp/refdata");
+        } catch (IOException | InterruptedException exception) {
+            exception.printStackTrace();
+        }
+        return session;
+    }
+
+    private static Request createChainRequest(Service service, String underlier, ZonedDateTime asOf) {
+        Request request = service.createRequest("ReferenceDataRequest");
+        request.getElement("securities").appendValue(underlier);
+        request.getElement("fields").appendValue("OPT_CHAIN");
+        Element asOfOverride = request.getElement("overrides").appendElement();
+        asOfOverride.setElement("fieldId","SINGLE_DATE_OVERRIDE");
+        asOfOverride.setElement("value", dateToString(asOf));
+
+        return request;
+    }
+
+    private static String dateToString(ZonedDateTime date) {
+        int year = date.getYear();
+        int month = date.getMonthValue();
+        int day = date.getDayOfMonth();
+        return year + (month < 10 ? "0" : "") + month + (day < 10 ? "0" : "") + day;
+    }
+
+    private static ZonedDateTime stringToDate(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuuMMdd HHmmss VV");
+        return ZonedDateTime.parse(date + " " + EXPIRY_TIME + " " + TIME_ZONE, formatter);
     }
 }
